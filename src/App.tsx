985fc73a678acb52ff84d7d5e73bdc50ce263d4c
Moluno-xiii <molunoprogress@gmail.com>
2025-01-28 21:13:00 +0100

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-dvh flex-col items-center justify-center text-2xl">
+      <span className="flex font-bold text-black">
+        Richie Portfolio website
+      </span>
+    </div>
+  );
+}
+
+export default App;
